4306accd747bbb7cf1112301f0a04c55c5621ac1
Sneha Mahajan <sneh.km@gmail.com>
2022-05-16 18:18:07 -0400

    Reformat character counter code.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,10 @@ $(document).ready(() => {
   $("#tweet-text").keyup(function () {
     const typedText = $(this).val();
     const numChars = 140 - typedText.length;
-    $("#charCounter").text(numChars);
-    if (numChars <= 0) {
-      $("#charCounter").css("color", "red");
-    } else {
-      $("#charCounter").css("color", "#545149");
-    }
+    const counter = "#tweet-text + div output";
+
+    $(counter).text(numChars);
+    if (numChars < 0) $(counter).addClass("text-color-red");
+    else $(counter).removeClass("text-color-red");
   });
 });
