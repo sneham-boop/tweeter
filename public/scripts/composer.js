b1a307e6f37de624ac3facbb2ed3ecd8b218700e
Sneha Mahajan <sneh.km@gmail.com>
2022-05-19 16:52:30 -0400

    Fix behavior of go up button.

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
-  
+
   // Scroll up button on footer
   $(window).scroll(() => {
-    const scrollPosition = $(window).scrollTop();
+    let scrollPosition = $(window).scrollTop();
     $("#footer a").show();
     if (scrollPosition < 70) {
       $("#footer a").hide();
     }
   });
+
+  // Scroll up and focus on tweet text when up 
+  // button is pressed.
+  $("#footer a").click(()=>{
+    $(window).scrollTop(200);
+    $("#tweet-text").focus();
+  })
 });
