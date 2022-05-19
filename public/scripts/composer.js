d4f147b9d9bc2f155c3ed4972eb43cfcf16498f7
Sneha Mahajan <sneh.km@gmail.com>
2022-05-19 16:44:49 -0400

    Fix visibility of go up button on footer.

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+  
   // Scroll up button on footer
   $(window).scroll(() => {
-    $("#footer a").fadeTo(100, 1, function () {
-      $(this).fadeTo(100, 0.5);
-    });
+    const scrollPosition = $(window).scrollTop();
+    $("#footer a").show();
+    if (scrollPosition < 70) {
+      $("#footer a").hide();
+    }
   });
 });
