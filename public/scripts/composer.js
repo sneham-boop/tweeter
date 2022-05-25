b34b660e96633f9cd3afc4ca96bc5527c5806037
Sneha Mahajan <sneh.km@gmail.com>
2022-05-25 17:37:35 -0400

    Fix minor formatting errors.

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -2,12 +2,9 @@ $(document).ready(function () {
   // Scroll up button on footer
   $(window).scroll(() => {
     let scrollPosition = $(window).scrollTop();
-    
+
     if (scrollPosition < 70) $("#footer a").hide();
     else $("#footer a").show();
-
-    // if (scrollPosition > 373) $(".nav").fadeOut("fast");
-    // else $(".nav").show();
   });
 
   // Scroll up and focus on tweet text when up
@@ -18,7 +15,7 @@ $(document).ready(function () {
     $("#tweet-text").focus();
   });
 
-  //
+  // Begin writing tweet when double arrow pressed
   $(".nav-items a").click(() => {
     $(".new-tweet").slideToggle(200, "linear", () => {
       $("#tweet-text").focus();
